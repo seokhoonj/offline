c161006db3e6610af124a0979dae22b78fcc08db
seokhoonj <seokhoonj@gmail.com>
2023-08-20 17:06:30 +0900

    `link_tools` and `download_tools` functions are added

--- R/tools.R
+++ R/tools.R
@@ -1,8 +1,8 @@
-##' Get links of tool list for offline projects
+##' Links of tools for offline projects
 ##'
 ##' @export
-get_project_tools <- function() {
-  tool_list = list(
+link_tools <- function() {
+  tool_list <- list(
     "Download R" = "https://cran.r-project.org/bin/windows/base/",
     "Download Rtools" = "https://cran.r-project.org/bin/windows/Rtools/",
     "Download RStudio" = "https://posit.co/download/rstudio-desktop/",
@@ -16,4 +16,22 @@ get_project_tools <- function() {
       url = tool_list[i]
     ), "\n")
   }
+}
+
+##' Download tools for offline projects
+##'
+##' @param tools A string vector specifying tools
+##' @export
+download_tools <- function(tools = c("R", "Rtools", "RStudio")) {
+  tool_list <- list(
+    R = "https://cran.r-project.org/bin/windows/base/R-4.3.1-win.exe",
+    Rtools = "https://cran.r-project.org/bin/windows/Rtools/rtools43/files/rtools43-5550-5548.exe",
+    RStudio = "https://download1.rstudio.org/electron/windows/RStudio-2023.06.1-524.exe"
+  )
+  op <- options(timeout = 300)
+  on.exit(options(op))
+  invisible(lapply(
+    tool_list[names(tool_list) %in% tools],
+    function(x) download.file(x, basename(x))
+  ))
 }
